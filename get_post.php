aaded1dcc8177531cfa371b7e3011017296fde4f
seth@mondido <sethnejame>
2019-10-05 18:28:35 +0200

    Adding get_post w/ get/post request info

--- get_post.php
+++ get_post.php
@@ -1,3 +1,23 @@
+<?php
+  // $_GET is an array . . .data is sent in the URL of the HTTP req
+  if(isset($_GET['name'])) {
+    $name = htmlentities($_GET['name']);
+    echo $name;
+  }
+  // $_POST is an array . . .data is sent in the body of the HTTP req
+  if(isset($_POST['name'])) {
+    $name = htmlentities($_POST['name']);
+    echo $name;
+  }
+  // $_REQUEST is an array(SUPERGLOBAL)- data from the HTTP req can be retrieved for any method (GET/POST)
+  if(isset($_REQUEST['name'])) {
+    $name = htmlentities($_REQUEST['name']);
+    echo $name;
+  }
+
+  // $_SERVER is an array(SUPERGLOBAL)-
+  echo $_SERVER['QUERY_STRING'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +30,7 @@
 <body>
   <div class="container mt-4">
     <h1>Info Form</h1>
-  <form>
+  <form method="POST" action="get_post.php">
     <div class="form-group">
       <label for="name">Name</label><br>
       <input class="form-control" type="text" name="name" placeholder="Enter name">
@@ -21,5 +41,13 @@
     </div>
     <input type="submit" value="Submit" class="btn btn-primary">
   </form>
+  <ul>
+    <li>
+      <a href="get_post.php?name=Seth">Seth</a>
+      <a href="get_post.php?name=Natalie">Natalie</a>
+      <a href="get_post.php?name=Charlie">Charlie</a>
+    </li>
+  </ul>
+  <h1><?php echo "{$name}'s Profile"; ?></h1>
 </body>
 </html>
