f11df2b3d5c25e93a4f00de15a06a2026fa72f35
seth@mondido <sethnejame>
2019-10-07 10:42:45 +0200

    Adding PHP Sessions page

---
+++ website4/page1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PHP Sessions</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container mt-4">
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+      <h1>PHP Sessions - Page 1</h1>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" name="name" placeholder="Name">
+      </div>
+      <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="text" class="form-control" name="email" placeholder="Email">
+      </div>
+      <button type="submit" name="submit" value="Submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</body>
+</html>
