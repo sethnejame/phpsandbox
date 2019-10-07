5e868e46e5bf187d3c7a27de0400094e7e027b57
seth@mondido <sethnejame>
2019-10-07 09:25:07 +0200

    Adding more form fields, navbar & styling

--- website3/index.php
+++ website3/index.php
@@ -11,19 +11,32 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
-<div class="container mt-4">
-  <form method="post" action="index.php">
-    <h1>Contact Us</h1>
-    <div class="form-group">
-      <label for="name">Name</label>
-      <input type="text" class="form-control" name="name" placeholder="Name">
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">Contact Us</a>
+      </div>
     </div>
-    <div class="form-group">
-      <label for="email">E-mail</label>
-      <input type="text" class="form-control" name="email" placeholder="Email">
-    </div>
-    <button type="submit" name="Submit" class="btn btn-primary">Submit</button>
-  </form>
-</div>
+  </nav>
+
+  <div class="container mt-4">
+    <form method="post" action="">
+      <h1>Contact Us</h1>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" value="" name="name" placeholder="Name">
+      </div>
+      <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="text" class="form-control" value="" name="email" placeholder="Email">
+      </div>
+      <div class="form-group">
+        <label for="message">Message</label>
+        <textarea class="form-control" name="message" placeholder="Leave a message. . .">
+        </textarea>
+      </div>
+      <button type="submit" name="Submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 </body>
 </html>
